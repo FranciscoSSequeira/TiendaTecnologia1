e94d72316c157ded709bb4347a4af25a9108b0ab
Francisco Sequeira <franciscosequeira879@gmail.com>
2026-01-16 17:47:35 -0300

    modificacion use

--- front/pages/productos.js
+++ front/pages/productos.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-'use client';
 
 export default function Productos() {
   const [products, setProducts] = useState([]);
