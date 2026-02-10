2e30aad5c549f387177e18e78df86666c38f01c9
Francisco Sequeira <franciscosequeira879@gmail.com>
2026-02-10 20:31:38 -0300

    agregado modo dark

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,6 +2,20 @@ import Link from "next/link";
 import Carrusel from "../components/Carrusel/Carrusel";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = React.useState(false);
+  React.useEffect(() => {
+    // Leer preferencia al cargar
+    const savedMode = typeof window !== 'undefined' ? localStorage.getItem('darkMode') : null;
+    if (savedMode) {
+      setDarkMode(savedMode === 'true');
+    }
+  }, []);
+  React.useEffect(() => {
+    document.body.className = darkMode ? 'dark-mode' : '';
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('darkMode', darkMode);
+    }
+  }, [darkMode]);
   return (
     <div className="container">
       <header className="header">
@@ -9,9 +23,12 @@ export default function Home() {
         <nav className="nav">
           <Link href="/">Inicio</Link>
           <Link href="/productos">Productos</Link>
-          <Link href="/carrito">Carrito</Link> {/* <-- Agregado */}
+          <Link href="/carrito">Carrito</Link> 
           <Link href="/contacto">Contacto</Link>
         </nav>
+        <button style={{marginLeft: '20px'}} onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? 'Modo White' : 'Modo Dark'}
+        </button>
       </header>
       <Carrusel />
       <section className="hero">
